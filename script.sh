7e5f622316ced5354072a276f471261e2f1abc6e
Christopher Tarkaa <88291960+christarkaa@users.noreply.github.com>
2024-03-29 08:40:17 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -6,6 +6,43 @@ mkdir covid && cd "$_"
 # Download the dataset 
 fastq-dump --split-files ERR5743893
 
+# Make a new directory for quality control reports
+mkdir -p QC_Reports 
+
+# Quality control
+fastqc ERR5743893_1.fastq ERR5743893_2.fastq -o QC_Reports
+
+# Use MultiQC to summarise the QC results
+multiqc . 
+
+# The reads are good so there is no need for trimming
+
+# Make a new directory "Mapping"
+mkdir Mapping
+
+# Index the reference genom
+bwa index MN908947.fasta 
+
+# Mapping
+bwa mem MN908947.fasta ERR5743893_1.fastq ERR5743893_2.fastq > Mapping/ERR5743893.sam
+
+# Convert the SAM file to a BAM file to save space
+samtools view -@ 20 -S -b Mapping/ERR5743893.sam > Mapping/ERR5743893.bam
+
+# Index the BAM file
+samtools index Mapping/ERR5743893.sorted.bam
+
+# Index the reference genome using samtools
+samtools faidx MN908947.fasta
+
+# Variant calling using freebayes
+freebayes -f MN908947.fasta Mapping/ERR5743893.sorted.bam  > ERR5743893.vcf
+
+# Compres the VCF file
+bgzip ERR5743893.vcf
+
+# Index the VCF file
+tabix ERR5743893.vcf.gz
 
 
 
