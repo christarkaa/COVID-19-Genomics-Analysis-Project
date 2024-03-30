7b6bd7d0e15d8910aaabfd27504e300229c88b94
Christopher Tarkaa <88291960+christarkaa@users.noreply.github.com>
2024-03-30 05:52:00 +0100

    Update nextflow.sh

--- nextflow.sh
+++ nextflow.sh
@@ -1,1 +1,18 @@
+#!/usr/bin/env bash
+
+# Create a sample text file and copy and paste the Ascencion numbers
+# Define the ENA accessions
+accessions=(
+  "ERR5556343"
+  "SRR13500958"
+  "ERR5743893"
+  "ERR5181310"
+  "ERR5405022"
+)
+
+# Create samples.txt and copy accessions into it
+echo "${accessions[@]}" > samples.txt
+
+echo "File samples.txt created with the ENA accessions."
+
 
